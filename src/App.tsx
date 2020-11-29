43052a88011a210e7c2cd1f815f2ae367c0ddc14
Andrea Roveroni <roveroniandrea@gmail.com>
2020-11-29 13:51:33 +0100

    removed useless files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import './App.css';
 import MainRouter from './mainRouter/MainRouter';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <MainRouter></MainRouter>
-        </header>
-      </div>
+      <MainRouter></MainRouter>
     );
   }
 }
