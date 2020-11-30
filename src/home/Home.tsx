9d2e5961fcb0736ecf1ecc65afec71859aee0ef1
Andrea Roveroni <roveroniandrea@gmail.com>
2020-11-30 16:21:37 +0100

    facebook get posts fix

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,6 +1,15 @@
 import React from "react";
+import Utility from "../utility/Utility";
 
 class Home extends React.Component {
+
+    componentDidMount() {
+        Utility.getFacebookPosts()
+            .then(posts => {
+                console.log(posts);
+            })
+    }
+
     render() {
         return (
             <h1>Home page!</h1>
--- src/utility/Utility.tsx
+++ src/utility/Utility.tsx
@@ -4,9 +4,16 @@ export interface EventData {
     eventId: string,
     eventName: string
 }
+
+export interface FacebookPost {
+    date: Date,
+    id: string,
+    message: string
+}
 export default class Utility {
 
     private static eventbriteEndpoint = 'https://www.eventbriteapi.com/v3';
+    private static facebookEndpoint = 'https://graph.facebook.com/v9.0';
 
     static getCurrentYears(): string {
         const curr = new Date();
@@ -52,5 +59,26 @@ export default class Utility {
                 })
                 .catch(() => reject());
         });
+    }
+
+    static getFacebookPosts(): Promise<FacebookPost[]> {
+        return new Promise((resolve, reject) => {
+            Axios.get(`${this.facebookEndpoint}/${process.env.REACT_APP_FACEBOOK_PAGE_ID}/feed?method=get&pretty=0&sdk=joey&suppress_http_code=1&access_token=${process.env.REACT_APP_FACEBOOK_ACCESS_TOKEN}`)
+                .then(response => {
+                    if (response.data.data) {
+                        resolve(response.data.data.map(((d: { id: any; message: any; creation_time: any; }) => {
+                            const post: FacebookPost = {
+                                id: d.id,
+                                message: d.message,
+                                date: d.creation_time
+                            }
+                            return post;
+                        })))
+                    }
+                    else {
+                        reject();
+                    }
+                });
+        })
     }
 }
