77f7c851b32584afd218f5cb4818abbb12589237
Andrea Roveroni <roveroniandrea@gmail.com>
2020-12-16 12:16:53 +0100

    styled home

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,10 +1,20 @@
-import {  useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import FacebookPost from './facebookPost/FacebookPost';
 import { FacebookAPI, FacebookPostData } from '../utility/FacebookAPI';
-import style from './Home.module.css';
 import { EventBriteAPI, EventData } from '../utility/EventbriteAPI';
 import { Utility } from '../utility/Utility';
 import { useHistory } from 'react-router';
+
+const styles: {
+    [ elem: string ]: CSSProperties;
+} = {
+    columnPosts: { display: 'flex', justifyContent: 'center', flexWrap: 'wrap' },
+    eventDiv: {
+        height: 'auto',
+        width: 'auto',
+        marginBottom: '5%',
+    }
+};
 
 export default function Home() {
 
@@ -28,14 +38,14 @@ export default function Home() {
 
     return (
         <div className="columns is-desktop">
-            <div className={ `column is-three-quarters ${style.postColumn}` } style={ ({ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }) }>
+            <div className='column is-three-quarters' style={ styles.columnPosts}>
                 { posts.map(post => <FacebookPost key={ post.fullId } post={ post } />) }
             </div>
             <div className="column">
                 <div className="box">
                     <h1 className="title">Prossimi eventi</h1>
                     { recentEvents.map((ev: EventData) => (
-                        <div className={ "button is-large " + style.eventButton } key={ ev.eventId } onClick={ () => Utility.navigateToEvent(ev, history) }>
+                        <div className='button is-large' style={ styles.eventDiv } key={ ev.eventId } onClick={ () => Utility.navigateToEvent(ev, history) }>
                             <div className="block">
                                 <p className="title is-4">{ ev.eventName }</p>
                                 <p className="subtitle is-6">{ Utility.formatDate(ev.start) }</p>
