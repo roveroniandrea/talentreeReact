fabd380d1019801ac300da03c9d6c39475250e5a
Andrea Roveroni <roveroniandrea@gmail.com>
2020-12-16 12:19:59 +0100

    MainRouter functional component

--- src/mainRouter/MainRouter.tsx
+++ src/mainRouter/MainRouter.tsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,36 +6,31 @@ import {
 import EventPage from "../eventPage/EventPage";
 import Home from "../home/Home";
 import AppBar from "../AppBar/AppBar";
-import TalentreePage  from '../talentreePage/TalentreePage';
+import TalentreePage from '../talentreePage/TalentreePage';
 import { YoutubeVideos } from '../youtubeVideos/YoutubeVideos';
 
-class MainRouter extends Component {
-
-    render() {
-        return (
-            <Router>
-                <AppBar/>
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route exact path="/talentree">
-                        <TalentreePage/>
-                    </Route>
-                    <Route path="/event/:eventId">
-                        <EventPage />
-                    </Route>
-                    <Route exact path="/gallery">
-                        <YoutubeVideos />
-                    </Route>
-                    <Route path="*">
-                        <h1>Not implemented</h1>
-                        {/*<Redirect to="/" />*/}
-                    </Route>
-                </Switch>
-            </Router>
-        );
-    }
-}
-
-export default MainRouter;
+export default function MainRouter() {
+    return (
+        <Router>
+            <AppBar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/talentree">
+                    <TalentreePage />
+                </Route>
+                <Route path="/event/:eventId">
+                    <EventPage />
+                </Route>
+                <Route exact path="/gallery">
+                    <YoutubeVideos />
+                </Route>
+                <Route path="*">
+                    <h1>Not implemented</h1>
+                    {/*<Redirect to="/" />*/ }
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
