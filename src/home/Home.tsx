0127541fe7b59db960238f8bbb1455f30552d591
Andrea Roveroni <roveroniandrea@gmail.com>
2020-12-16 12:08:30 +0100

    Home functional component

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,66 +1,49 @@
-import { Component } from "react";
+import {  useEffect, useState } from "react";
 import FacebookPost from './facebookPost/FacebookPost';
 import { FacebookAPI, FacebookPostData } from '../utility/FacebookAPI';
 import style from './Home.module.css';
 import { EventBriteAPI, EventData } from '../utility/EventbriteAPI';
 import { Utility } from '../utility/Utility';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
-import { StaticContext } from 'react-router';
+import { useHistory } from 'react-router';
 
-interface HomeState {
-    posts: FacebookPostData[];
-    recentEvents: EventData[];
-}
-class Home extends Component<RouteComponentProps, HomeState> {
+export default function Home() {
 
-    constructor(props: RouteComponentProps<{}, StaticContext, unknown> | Readonly<RouteComponentProps<{}, StaticContext, unknown>>) {
-        super(props);
-        this.state = {
-            posts: [],
-            recentEvents: []
-        };
-    }
+    const history = useHistory();
+    const [ posts, setPosts ] = useState<FacebookPostData[]>([]);
+    const [ recentEvents, setRecentEvents ] = useState<EventData[]>([]);
 
-    componentDidMount() {
+    useEffect(() => {
         FacebookAPI.getFacebookPosts()
             .then(posts => {
-                this.setState({
-                    posts
-                });
+                setPosts(posts);
             });
 
         EventBriteAPI.getNextActivities()
             .then(events => {
                 //TODO: controlla correttezza
                 let nearest = events.sort((e1, e2) => e1.start.getTime() - e2.start.getTime());
-                this.setState({
-                    recentEvents: nearest.slice(0, 5)
-                });
+                setRecentEvents(nearest.slice(0, 5));
             });
-    }
+    }, []);
 
-    render() {
-        return (
-            <div className="columns is-desktop">
-                <div className={ `column is-three-quarters ${style.postColumn}` } style={ ({ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }) }>
-                    { this.state.posts.map(post => <FacebookPost key={ post.fullId } post={ post } />) }
-                </div>
-                <div className="column">
-                    <div className="box">
-                        <h1 className="title">Prossimi eventi</h1>
-                        { this.state.recentEvents.map((ev: EventData) => (
-                            <div className={ "button is-large " + style.eventButton } key={ ev.eventId } onClick={ () => Utility.navigateToEvent(ev, this.props.history) }>
-                                <div className="block">
-                                    <p className="title is-4">{ ev.eventName }</p>
-                                    <p className="subtitle is-6">{ Utility.formatDate(ev.start) }</p>
-                                </div>
+    return (
+        <div className="columns is-desktop">
+            <div className={ `column is-three-quarters ${style.postColumn}` } style={ ({ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }) }>
+                { posts.map(post => <FacebookPost key={ post.fullId } post={ post } />) }
+            </div>
+            <div className="column">
+                <div className="box">
+                    <h1 className="title">Prossimi eventi</h1>
+                    { recentEvents.map((ev: EventData) => (
+                        <div className={ "button is-large " + style.eventButton } key={ ev.eventId } onClick={ () => Utility.navigateToEvent(ev, history) }>
+                            <div className="block">
+                                <p className="title is-4">{ ev.eventName }</p>
+                                <p className="subtitle is-6">{ Utility.formatDate(ev.start) }</p>
                             </div>
-                        )) }
-                    </div>
+                        </div>
+                    )) }
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default withRouter(Home);
