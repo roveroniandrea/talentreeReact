6143695baa90b2a8e5adee19fb8803345741d0c5
Andrea Roveroni <roveroniandrea@gmail.com>
2020-12-16 12:23:09 +0100

    TalentreePage functional component

--- src/talentreePage/TalentreePage.tsx
+++ src/talentreePage/TalentreePage.tsx
@@ -1,42 +1,28 @@
-import React, { Component, Fragment } from 'react';
-import { StaticContext } from 'react-router';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
+import React, { Fragment, useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import { EventBriteAPI, EventData } from '../utility/EventbriteAPI';
 import { Utility } from '../utility/Utility';
 
-interface TalentreePageState {
-    talentreeEvents: EventData[];
-}
-class TalentreePage extends Component<RouteComponentProps, TalentreePageState> {
+export default function TalentreePage() {
+    const [ talentreeEvents, setTalentreeEvents ] = useState<EventData[]>([]);
 
-    constructor(props: RouteComponentProps<{}, StaticContext, unknown> | Readonly<RouteComponentProps<{}, StaticContext, unknown>>) {
-        super(props);
-        this.state = {
-            talentreeEvents: []
-        };
-    }
+    const history = useHistory();
 
-    componentDidMount() {
+    useEffect(() => {
         EventBriteAPI.getTalentreeEvents()
-            .then(events => this.setState({
-                talentreeEvents: events
-            }));
-    }
+            .then(events => setTalentreeEvents(events));
+    }, []);
 
-    render() {
-        return (
-            <Fragment>
-                { this.state.talentreeEvents.map((ev: EventData) => (
-                            <div className={ "button is-large " } key={ ev.eventId } onClick={ () => Utility.navigateToEvent(ev, this.props.history) }>
-                                <div className="block">
-                                    <p className="title is-4">{ ev.eventName }</p>
-                                    <p className="subtitle is-6">{ Utility.formatDate(ev.start) }</p>
-                                </div>
+    return (
+        <Fragment>
+            { talentreeEvents.map((ev: EventData) => (
+                        <div className={ "button is-large " } key={ ev.eventId } onClick={ () => Utility.navigateToEvent(ev, history) }>
+                            <div className="block">
+                                <p className="title is-4">{ ev.eventName }</p>
+                                <p className="subtitle is-6">{ Utility.formatDate(ev.start) }</p>
                             </div>
-                        )) }
-            </Fragment>
-        );
-    }
+                        </div>
+                    )) }
+        </Fragment>
+    );
 }
-
-export default withRouter(TalentreePage);
