a78170b5bbe0a1968647082a91cecf2baec9c37d
Andrea Roveroni <roveroniandrea@gmail.com>
2020-12-17 18:24:46 +0100

    some mentors

--- src/mentors/MentorsPage.tsx
+++ src/mentors/MentorsPage.tsx
@@ -2,6 +2,7 @@ import mentors from '../core/mentors.json';
 import MentorBox from './mentorBox/MentorBox';
 
 export default function MentorsPage() {
+    mentors.sort((m1, m2) => m1.name.localeCompare(m2.name));
     return (
         <div>
             {mentors.map((mentor, i) => <MentorBox key={ i } mentor={ mentor } />) }
--- src/mentors/mentorBox/MentorBox.tsx
+++ src/mentors/mentorBox/MentorBox.tsx
@@ -1,20 +1,17 @@
 export default function MentorBox(props: { mentor: { name: string, description: string, image: string; }; }) {
     return (
-        <div className="card">
+        <div className="card" style={ ({ margin: '20px' }) }>
             <div className="card-content">
                 <div className="media">
                     <div className="media-left">
-                        <figure className="image is-48x48">
-                            <img src={ `${process.env.PUBLIC_URL}/assets/${props.mentor.image}` } alt={ props.mentor.name } />
+                        <figure className="image is-128x128 is-square">
+                            <img className="is-rounded" src={ `${process.env.PUBLIC_URL}/assets/${props.mentor.image}` } alt={ props.mentor.name } />
                         </figure>
                     </div>
-                    <div className="media-content">
-                        <p className="title is-4">{ props.mentor.name }</p>
+                    <div className="media-content" style={ ({ marginLeft: '10px' }) }>
+                        <p className="title is-3">{ props.mentor.name }</p>
+                        <p className="subtitle is-5">{ props.mentor.description }</p>
                     </div>
-                </div>
-
-                <div className="content">
-                    { props.mentor.description }
                 </div>
             </div>
         </div>
