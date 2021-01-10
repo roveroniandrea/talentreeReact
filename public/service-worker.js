1c9e59d35216dc8795985cc7cef5be2d1ca14478
Andrea Roveroni <roveroniandrea@gmail.com>
2021-01-10 14:04:38 +0100

    service worker seems ok

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,30 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
-// Caching images
-workbox.routing.registerRoute(({ request }) => request.destination === 'image', new workbox.strategies.CacheFirst());
-
-// Caching styles
-workbox.routing.registerRoute(({ request }) => request.destination === 'style', new workbox.strategies.CacheFirst());
-
-//Caching scripts external to this origin
 workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'script' && !request.url.includes(origin),
-    new workbox.strategies.CacheFirst()
+    ({ request }) => request.destination === 'image',
+    new workbox.strategies.CacheFirst({
+        cacheName: 'image-cache',
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'style',
+    new workbox.strategies.CacheFirst({
+        cacheName: 'style-cache',
+    })
+);
+
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'script',
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'scripts-cache',
+    })
+);
+
+// Chaching documents with Network first
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === 'document',
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'documents-cache',
+    })
 );
